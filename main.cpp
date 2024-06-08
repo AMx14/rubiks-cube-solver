b8f6a9dee8f1435fbe8d53c7e799159ca353df38
AMx14 <maithaniaxat@gmail.com>
2024-06-08 12:53:26 +0530

    Organized in Model Directory

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
-
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <set>
+#include <utility>
+#include <cmath>
+#include <limits>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <list>
+#include <unordered_map>
+#include <unordered_set>
+#include <functional>
+#include <numeric>
+#include "Model/RubiksCube3dArray.cpp"
+using namespace std;
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    RubiksCube3dArray object3DArray;
+    object3DArray.print();
     return 0;
 }
