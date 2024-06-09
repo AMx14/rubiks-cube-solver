666a942d2d8042439a1bde778d8c390905226be0
AMx14 <maithaniaxat@gmail.com>
2024-06-09 20:29:06 +0530

    Added NibbleArray

---
+++ PatternDatabases/NibbleArray.h
@@ -0,0 +1,52 @@
+//
+// Created by Akshat Maithani on 09/06/24.
+//
+
+#ifndef RUBIKS_CUBE_SOLVER_NIBBLEARRAY_H
+#define RUBIKS_CUBE_SOLVER_NIBBLEARRAY_H
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <set>
+#include <utility>
+#include <cmath>
+#include <limits>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <list>
+#include <unordered_map>
+#include <unordered_set>
+#include <functional>
+#include <numeric>
+
+using namespace std;
+
+class NibbleArray {
+    size_t size;
+    vector<uint8_t> arr;
+
+public:
+    NibbleArray(const size_t size, const uint8_t val = 0xFF);
+
+    uint8_t get(const size_t pos) const;
+
+    void set(const size_t pos, const uint8_t val);
+
+    unsigned char *data();
+
+    const unsigned char *data() const;
+
+    size_t storageSize() const;
+
+    void inflate(vector<uint8_t> &dest) const;
+
+    void reset(const uint8_t val = 0xFF);
+
+};
+
+#endif //RUBIKS_CUBE_SOLVER_NIBBLEARRAY_H
